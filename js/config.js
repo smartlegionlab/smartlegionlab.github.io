d774b60daed13b694a4a5d7f1bc5f9007bc6e4cb
A.A. Suvorov <smartlegiondev@gmail.com>
2025-10-11 04:18:01 +0700

    smartlegionlab.github.io v0.8.1

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@ const CONFIG = {
     DEVTO_USERNAME: 'smartlegionlab',
     EXCLUDED_REPOSITORIES: ['smartlegionlab.github.io', 'smartlegionlab'],
     RESEARCH_STATS: {
-        pointerParadigm: { views: 136, downloads: 132 },
-        localDataParadigm: { views: 56, downloads: 52 }
+        pointerParadigm: { views: 140, downloads: 135 },
+        localDataParadigm: { views: 60, downloads: 57 }
     },
     SCROLL_OFFSET: 80
 };
