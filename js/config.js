bb032d81b95f4897e70dcdffc921ca2038a8b59c
Alexander Suvorov <smartlegiondev@gmail.com>
2026-05-14 23:35:48 +0700

    feat: improve applications.html

--- js/config.js
+++ js/config.js
@@ -16,7 +16,7 @@ const CONFIG = {
         USERS_SUPPORTED: '500K+',
         COMMITS: "11K",
         PARADIGMS: 3,
-        APPLICATIONS: 20,
+        APPLICATIONS: 24,
         PUBLICATIONS: 4
     },
 
