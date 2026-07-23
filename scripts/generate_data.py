4d3b6d738f1d47abb82e52e2a27585a3ec8b1c28
Alexander Suvorov <smartlegiondev@gmail.com>
2026-07-24 06:39:28 +0700

    feat(applications): add smart-pip-collector.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -40,6 +40,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/applications/smart-2fa-manager-bash.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/applications/github-ssh-key.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/applications/todo-app-cli.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
+    {'loc': f"{BASE_URL}/applications/smart-pip-collector.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/team.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/profiles/smartlegionlab.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/profiles/aixandrolab.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
