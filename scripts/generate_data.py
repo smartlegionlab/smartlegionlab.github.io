31975167b3e25a7d4606d0eb6d8ac08e636a4598
Alexander Suvorov <smartlegiondev@gmail.com>
2026-07-24 06:22:02 +0700

    feat(applications): add smart-password-generator-csharp-cli.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -35,6 +35,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/applications/clipassman.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/applications/github-repos-backup-tools.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/applications/smart-password-manager-csharp-cli.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
+    {'loc': f"{BASE_URL}/applications/smart-password-generator-csharp-cli.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/team.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/profiles/smartlegionlab.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/profiles/aixandrolab.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
