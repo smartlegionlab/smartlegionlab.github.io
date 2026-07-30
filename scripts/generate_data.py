3ec4337f85b404dfcde0bc007bb7aeb04405ed78
Alexander Suvorov <smartlegiondev@gmail.com>
2026-07-30 11:15:26 +0700

    feat(libraries): add smartpasslib-csharp.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -35,6 +35,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/libraries/forgejo-sync-manager-core.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/smartpasslib-js.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/smartpasslib-kotlin.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
+    {'loc': f"{BASE_URL}/libraries/smartpasslib-csharp.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/ecosystems.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/ecosystems/smart-passwords-ecosystem.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/ecosystems/deterministic-ecosystem.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
