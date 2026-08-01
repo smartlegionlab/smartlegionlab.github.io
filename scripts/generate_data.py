73122d2c7604e16b71d66972089a9eba301903c3
Alexander Suvorov <smartlegiondev@gmail.com>
2026-08-01 10:40:50 +0700

    feat(projects): add smart-repository-manager-cli.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -13,6 +13,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/projects/django-smart-dynamic-path.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/smart-dynamic-path.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/smart-password-manager-desktop.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
+    {'loc': f"{BASE_URL}/projects/smart-repository-manager-cli.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/libraries.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/smartpasslib.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/clipassman.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
