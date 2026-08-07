dec16c9ebf4c002fdbda7b53b0c6517c51d0cb50
Alexander Suvorov <smartlegiondev@gmail.com>
2026-08-07 14:36:09 +0700

    feat(projects): add clitodo.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -42,6 +42,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/projects/pointer-based-security-paradigm.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/blockchain.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/smart-2fa-manager-android.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
+    {'loc': f"{BASE_URL}/projects/clitodo.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/libraries.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/smartpasslib.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/clipassman.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
