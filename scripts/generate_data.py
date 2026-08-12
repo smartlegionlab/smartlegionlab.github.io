7e39d1102ca268c040f137e1a92c3ca2be3019f2
Alexander Suvorov <smartlegiondev@gmail.com>
2026-08-12 14:58:03 +0700

    feat(projects): add smart-2fa-manager-cli.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -57,6 +57,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/projects/smartprinter.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/commandex.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/smart-2fa-manager-bash.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
+    {'loc': f"{BASE_URL}/projects/smart-2fa-manager-cli.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/libraries.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/smartpasslib.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/clipassman.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
