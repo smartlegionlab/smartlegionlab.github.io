bc5577a36a527e404ff53d99cab0958ef931b426
Alexander Suvorov <smartlegiondev@gmail.com>
2026-08-12 15:28:53 +0700

    feat(projects): add todo-app-tg-bot.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -63,6 +63,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/projects/smart-tsp-benchmark.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/smart-password-generator.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/personal-telegram-bot.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
+    {'loc': f"{BASE_URL}/projects/todo-app-tg-bot.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/libraries.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/smartpasslib.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/clipassman.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
