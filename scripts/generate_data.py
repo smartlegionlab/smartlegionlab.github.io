57050c260d3fe1cc5d949a7b6fa726a0f01926f3
Alexander Suvorov <smartlegiondev@gmail.com>
2026-08-12 16:53:24 +0700

    feat(projects): add github-repos-backup-tools.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -75,6 +75,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/projects/smart-task-manager.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/smart-text-randomizer.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/smart-pip-collector.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
+    {'loc': f"{BASE_URL}/projects/github-repos-backup-tools.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/libraries.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/smartpasslib.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/clipassman.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
