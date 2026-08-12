929869e3e44d48e3a341713674ea709acae42ef8
Alexander Suvorov <smartlegiondev@gmail.com>
2026-08-12 16:48:39 +0700

    feat(projects): add smart-text-randomizer.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -73,6 +73,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/projects/smart-babylon-library.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/babylonian-image-library.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/smart-task-manager.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
+    {'loc': f"{BASE_URL}/projects/smart-text-randomizer.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/libraries.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/smartpasslib.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/clipassman.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
