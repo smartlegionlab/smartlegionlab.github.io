4d2a2eb74181124c431f5c904ca66fb34cfb9ba8
Alexander Suvorov <smartlegiondev@gmail.com>
2026-08-14 17:01:06 +0700

    feat(projects): add todo-app-web-version.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -98,6 +98,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/projects/official-tg-bot.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/get-ssh-ip.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/todo-tg-bot.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
+    {'loc': f"{BASE_URL}/projects/todo-app-web-version.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/libraries.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/smartpasslib.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/clipassman.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
