3f23f115d6057ab53ec7a71879e29c4ffef2cf80
Alexander Suvorov <smartlegiondev@gmail.com>
2026-08-14 16:44:46 +0700

    feat(projects): add smart-bug-tracker.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -93,6 +93,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/projects/githubinformer.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/outputdecorator.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/projects/smart-guitar-tuner-android.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
+    {'loc': f"{BASE_URL}/projects/smart-bug-tracker.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/libraries.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/smartpasslib.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/libraries/clipassman.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
