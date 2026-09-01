226388118df1f9ae2e6442c0ddd8b44ee239c97e
Alexander Suvorov <smartlegiondev@gmail.com>
2026-09-01 09:20:33 +0700

    feat(applications): add smart-notebook.html

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -181,6 +181,7 @@ BASE_PAGES = [
     {'loc': f"{BASE_URL}/applications/todo-app-web-version.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/applications/get_ssh_ip.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/applications/android-todo-app.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
+    {'loc': f"{BASE_URL}/applications/smart-notebook.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.9'},
     {'loc': f"{BASE_URL}/team.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/profiles/smartlegionlab.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
     {'loc': f"{BASE_URL}/profiles/aixandrolab.html", 'lastmod': '2026-07-01', 'changefreq': 'weekly', 'priority': '0.8'},
